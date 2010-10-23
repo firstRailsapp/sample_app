dc3d2299257b13deed8f20b6f3a1f9fe7042f4e5
angamuthu <angamuthu.g@multunus.com>
2010-10-23 17:26:49 +0530

    User signup complete

---
+++ spec/integration/users_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Users" do
+  describe "signup" do
+    describe "failure" do
+      it "should not make a new user" do
+        lambda do
+          visit signup_path
+          click_button
+          response.should render_template('new')
+          response.should have_tag('div#errorExplanation')
+        end.should_not change(User, :count)
+      end
+
+      describe "success" do
+        it "should make a new user" do
+          lambda do
+            visit signup_path
+            fill_in "Name", :with => "example user"
+            fill_in "Email", :with => "user@example.com"
+            fill_in "Password", :with => "foobar"
+            fill_in "confirmation", :with => "foobar"
+            click_button
+            response.should render_template('users/show')
+          end.should change(User,:count).by(1)
+        end
+      end
+    end
+  end
+                                 
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,13 @@ require 'spec/autorun'
 require 'spec/rails'
 
 # Uncomment the next line to use webrat's matchers
-#require 'webrat/integrations/rspec-rails'
+# require 'webrat/integrations/rspec-rails'
+require 'spec/rails'
+require 'webrat'
 
+Webrat.configure do |config|
+  config.mode = :rails
+end
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
